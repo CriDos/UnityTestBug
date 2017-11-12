bb2bd3745935ba1b5b2091d9019de95011f63eb8
CriDos <CriDos@ya.ru>
2017-11-13 00:22:28 +0300

    first

---
+++ Assets/FindBug/FindBug.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace HardWorld.FindBug
+{
+    [InitializeOnLoad]
+    public static class FindBug
+    {
+        private const string StartTest = "StartTest";
+        private static readonly float OldTime;
+
+        static FindBug()
+        {
+            OldTime = Time.realtimeSinceStartup;
+            EditorApplication.update += Update;
+        }
+
+        private static void Update()
+        {
+            var deltaTime = Time.realtimeSinceStartup - OldTime;
+
+            if (EditorPrefs.GetBool(StartTest))
+            {
+                if (deltaTime >= 0)
+                {
+                    EditorApplication.isPlaying = !EditorApplication.isPlaying;
+                }
+            }
+        }
+
+        [MenuItem("Test/Stop %s")]
+        static void Stop()
+        {
+            EditorPrefs.SetBool(StartTest, false);
+        }
+
+        [MenuItem("Test/Start")]
+        static void Start()
+        {
+            EditorPrefs.SetBool(StartTest, true);
+            EditorSceneManager.OpenScene("Assets/FindBug/TestBug.unity");
+            EditorApplication.isPlaying = true;
+        }
+    }
+}
---
+++ Assets/FindBug/TestBug.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HardWorld
+{
+    public sealed class Chunk : MonoBehaviour
+    {
+    }
+
+    public class TestBug : MonoBehaviour
+    {
+        readonly Dictionary<Vector3Int, Tuple<GameObject, Chunk>> _chunks = new Dictionary<Vector3Int, Tuple<GameObject, Chunk>>();
+
+        private void Start()
+        {
+            for (byte x = 0; x < 24; x++)
+            for (byte y = 0; y < 24; y++)
+            for (byte z = 0; z < 24; z++)
+            {
+                var go = new GameObject();
+                go.transform.parent = transform;
+                _chunks.Add(new Vector3Int(x, y, z), Tuple.Create(go, go.AddComponent<Chunk>()));
+            }
+        }
+    }
+}
